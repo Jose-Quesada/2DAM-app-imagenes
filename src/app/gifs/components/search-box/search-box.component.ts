d7e9910a4e80860e751d854cd4f1730d20f936e0
Jose Quesada <jose.quesada@fernando3martos.com>
2024-10-29 09:12:27 +0100

    Servicio implementado

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gif.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -7,10 +8,28 @@ import { Component } from '@angular/core';
     <input type="text"
       class="form-control"
       placeholder="Buscar gifs..."
+      (keyup.enter)="searchBox()"
+      #txtTagInput
       >
   `,
 
 })
 export class SearchBoxComponent {
 
+  @ViewChild ('txtTagInput')
+  public tagInput !: ElementRef<HTMLInputElement>;
+
+  constructor( private gifService: GifsService){};
+
+
+  searchBox( ) {
+    const newTag = this.tagInput.nativeElement.value;
+
+    this.gifService.searchTag( newTag );
+
+    this.tagInput.nativeElement.value = '';
+
+    console.log({newTag});
+
+  }
 }
