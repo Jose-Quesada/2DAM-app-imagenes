2be9ef5a621fe6a2411f8c2ca165149ec2ba8cdf
Jose Quesada <jose.quesada@fernando3martos.com>
2024-10-31 10:02:46 +0100

    Petición HTTP completada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +19,7 @@ import { SharedModule } from './shared/shared.module';
     GifsModule,
     SharedModule,
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/gifs/services/gif.service.ts
+++ src/app/gifs/services/gif.service.ts
@@ -1,18 +1,58 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
+import { SearchResponse,Gif } from '../interfaces/gif.interface';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
-  private _tagHistory: string[] = [];
+  private _tagsHistory: string[] = [];
+  private apiKey: string = '9sRmUi7HCk50jblcNpzwSn4IE5hyi04t';
+  private serviceUrl:   string = 'Http://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  public gifList: Gif [] = [];
+
+  constructor(private http: HttpClient) {
+     }
+
+
 
   get tagHistory () {
-    return [...this._tagHistory];
+    return [...this._tagsHistory];
+  }
+
+  public organizedHistory (tag: string){
+
+    tag = tag.toLowerCase();
+
+    if (this._tagsHistory.includes(tag)){
+      this._tagsHistory = this._tagsHistory.filter ( (oldTag) => oldTag !== tag)
+    }
+
+    this._tagsHistory.unshift ( tag ) ;
+
+    this._tagsHistory = this._tagsHistory.splice(0,10);
+
+
   }
 
   public searchTag ( tag:string):void {
-    this._tagHistory.unshift( tag );
+
+    const params = new HttpParams()
+      .set ('api_key', this.apiKey)
+      .set ('limit', '10')
+      .set ('q', tag)
+
+
+    if (tag.length === 0) return;
+
+    this.organizedHistory (tag);
+
+    this.http.get<SearchResponse>(`${ this.serviceUrl }/search`, { params })
+      .subscribe ( (resp) => {
+        this.gifList = resp.data
+        console.log({gifs: this.gifList});
+      })
   }
 
 }
